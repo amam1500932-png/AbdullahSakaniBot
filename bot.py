a670a2a4d2a611a032a4d2a1adc2e3b5224c80df
amam1500932-png <amam1500932@gmail.com>
2025-12-05 16:55:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ import logging
 TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 CHAT_ID = os.environ.get("CHAT_ID")
 
-SAKANI_API_URL = “https://sakani.sa/api/web/lands/tax-incurred”
+SAKANI_API_URL = "https://sakani.sa/api/web/lands/tax-incurred"
 CHECK_INTERVAL = 300  # 5 دقائق
 
 # ================== إعداد السجلات ==================
