69b200ff4f175e2465eb1f982fa3824df56aaf60
amam1500932-png <amam1500932@gmail.com>
2025-12-05 21:04:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ logging.basicConfig(
 level=logging.INFO,
 format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
-logger = logging.getLogger(**name**)
+logger = logging.getLogger(__name__)
 
 previous_lands = {}
 
